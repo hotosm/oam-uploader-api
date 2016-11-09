beeabc1d9c01826d013fc8df31314a78539e1c10
Travis-CI <travis@somewhere.com>
2016-11-09 12:10:25 +0000

    CI deploy to gh-pages

---
+++ api_project.js
@@ -0,0 +1,27 @@
+define({
+  "title": "OAM API",
+  "name": "OpenAerialMap Uploader API",
+  "description": "",
+  "url": "https://oam-uploader-api.herokuapp.com",
+  "template": {
+    "withCompare": false
+  },
+  "header": {
+    "title": "Getting Started",
+    "content": ""
+  },
+  "order": [
+    "Meta",
+    "TMS",
+    "Analytics"
+  ],
+  "version": "1.0.0",
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2016-11-09T12:10:25.712Z",
+    "url": "http://apidocjs.com",
+    "version": "0.13.2"
+  }
+});
